b537214896148127eadd336ae1e9f0395001e4d2
Lalith Srinivas <lalithsrinivask@gmail.com>
2025-08-06 11:15:38 +0530

    Update address_test.go

--- address_test.go
+++ address_test.go
@@ -4,16 +4,4 @@ import (
 	"testing"
 )
 
-func TestGetLongitude(t *testing.T) {
-	long := Longitude()
-	if long > 180 || long < -180 {
-		t.Error("function Longitude need return a valid longitude")
-	}
-}
 
-func TestGetLatitude(t *testing.T) {
-	lat := Latitude()
-	if lat > 90 || lat < -100 {
-		t.Error("function Latitude need return a valid longitude")
-	}
-}
